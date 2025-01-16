6cdee9ca82e10f3782e582a24da5028c2927857e
EuRV <eugenirovensky@yandex.ru>
2025-01-16 17:24:42 +0600

    refactor: put the html code in the view

--- src/server/routes/home.clj
+++ src/server/routes/home.clj
@@ -1,15 +1,12 @@
 (ns server.routes.home
   (:require [compojure.core :refer [GET defroutes]]
-            [ring.util.response :refer [content-type response]])
+            
+            [server.view.home :refer [home-page]])
   (:gen-class))
 
 (defn home
-  [req]
-  (let [{:keys [uri request-method]} req]
-    (content-type (response (format "You requested %s %s"
-                                    (-> request-method name .toUpperCase)
-                                    uri))
-                  "text/plain")))
+  []
+  (home-page))
 
 (defroutes home-routes
-  (GET "/" request (home request)))
+  (GET "/" [] (home)))
